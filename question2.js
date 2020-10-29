233f04b4a015fd136e926ffbccbb1bb1a75416fa
durjoy07 <37486359+durjoy07@users.noreply.github.com>
2020-10-29 15:25:54 +0600

    Update question2.js

--- question2.js
+++ question2.js
@@ -1,30 +1,48 @@
-let notification = [
-  { message: "Lorem", read: true },
-  { message: "Ipsum", read: true },
-  { message: "Dolor", read: true },
-  { message: "Sit", read: false },
-  { message: "Amet", read: true },
+// Updated Code
+// Sorry, I didn't notice first that this problem has to be solved by using built-in higher order function
+// Now I have solved it using higher order function
+
+let notifications = [
+	{message: 'Lorem', read: true},
+	{message: 'Ipsum', read: true},
+	{message: 'Dolor', read: true},
+	{message: 'Sit', read: false},
+	{message: 'Amet', read: true}
 ];
 
 let allRead = true;
+allRead = notifications.every(currentValue => currentValue.read === true);
+console.log(allRead);
 
-let arrLength = notification.length - 1;
-let falseResult = 0;
 
-function arryMap(n, data) {
-  let valueObject = data[n];
-  if (!valueObject.read) {
-    falseResult++;
-  }
+//=============================== My Previous Solution ========================
+// let notification = [
+//   { message: "Lorem", read: true },
+//   { message: "Ipsum", read: true },
+//   { message: "Dolor", read: true },
+//   { message: "Sit", read: false },
+//   { message: "Amet", read: true },
+// ];
 
-  if (falseResult !== 0) {
-    allRead = false;
-    return console.log(allRead);
-  }
-  if (n === 0) {
-    return console.log(allRead);
-  }
-  return arryMap(n - 1, data);
-}
+// let allRead = true;
 
-arryMap(arrLength, notification);
+// let arrLength = notification.length - 1;
+// let falseResult = 0;
+
+// function arryMap(n, data) {
+//   let valueObject = data[n];
+//   if (!valueObject.read) {
+//     falseResult++;
+//   }
+
+//   if (falseResult !== 0) {
+//     allRead = false;
+//     return console.log(allRead);
+//   }
+//   if (n === 0) {
+//     return console.log(allRead);
+//   }
+//   return arryMap(n - 1, data);
+// }
+
+// arryMap(arrLength, notification);
